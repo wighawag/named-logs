8b1349ed87109a81be29dfff8d66234d0111e0f1
Wighawag <wighawag@gmail.com>
2020-08-28 08:33:23 +0100

    add dir and table

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 export type Logger = {
   assert(condition?: boolean, ...data: any[]): void;
-  debug(...data: any[]): void;
   error(...data: any[]): void;
+  warn(...data: any[]): void;
   info(...data: any[]): void;
   log(...data: any[]): void;
+  debug(...data: any[]): void;
+  dir(item?: any, options?: any): void;
+  table(tabularData?: any, properties?: string[]): void;
   trace(...data: any[]): void;
-  warn(...data: any[]): void;
 };
 type LoggerFactory = (namespace: string) => Logger;
 const noop = () => undefined;
 const noopLogger = {
   assert: noop,
-  debug: noop,
   error: noop,
+  warn: noop,
   info: noop,
   log: noop,
-  trace: noop,
-  warn: noop,
+  debug: noop,
+  dir: noop,
+  table: noop,
+  trace: noop
 };
 let func: LoggerFactory = () => noopLogger;
 export function hook(factory: LoggerFactory) {
